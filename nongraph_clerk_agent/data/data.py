a04a7ebdcc8d7595b91bb2f36b8256ceb93eefec
Yugandhar Reddy Akkisetty <43297534+yugandharreddyakkisetty@users.noreply.github.com>
2026-08-19 10:35:37 +0530

    Add files via upload

---
+++ nongraph_clerk_agent/data/data.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+students: dict[str, Any] = {
+    "14001": {
+        "name": "Yugandhar",
+        "age": 22,
+        "roll_number": "14001"
+    },
+    "14002": {
+        "name": "Manoj",
+        "age": 23,
+        "roll_number": "14002"
+    },
+    "14003": {
+        "name": "Nagesh",
+        "age": 24,
+        "roll_number": "14003"
+
+    }
+}
