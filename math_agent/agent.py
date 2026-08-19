367eaabc572eec9af8ed829954c52310ce77c00a
Yugandhar Reddy Akkisetty <43297534+yugandharreddyakkisetty@users.noreply.github.com>
2026-08-19 10:27:44 +0530

    Add files via upload

---
+++ math_agent/agent.py
@@ -0,0 +1,22 @@
+from langgraph.graph import StateGraph
+from math_agent.utils.state import  MessagesState
+from math_agent.utils.nodes import llm_call, tool_node, should_continue
+from langgraph.graph import START, END
+# Build workflow
+agent_builder = StateGraph(MessagesState)
+
+# Add nodes
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_conditional_edges(
+    "llm_call",
+    should_continue,
+    ["tool_node", END]
+)
+agent_builder.add_edge("tool_node", "llm_call")
+
+# Compile the agent
+agent = agent_builder.compile()
