310ad26171e5d1178e0f39ba63eb9ae9682290ba
Yugandhar Reddy Akkisetty <43297534+yugandharreddyakkisetty@users.noreply.github.com>
2026-08-19 10:26:19 +0530

    Delete math_agent directory

--- math_agent/state.py
+++
@@ -1,7 +0,0 @@
-from typing_extensions import TypedDict, Annotated
-from langchain.messages import AnyMessage
-import operator
-
-class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
-    llm_calls: int
